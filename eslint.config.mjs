8c6ada83d383596dc3cc1b87a61c931d31e1ee63
Max Stricker <max.stricker96@gmail.com>
2025-05-12 17:16:28 +0200

    added global prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js, prettier: pluginPrettier },
+    extends: ["js/recommended", "plugin:prettier/recommended"],
+    rules: {
+      "prettier/prettier": "error" // Zeigt Prettier-Fehler in ESLint an
+    }
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser }
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettier // Deaktiviert ESLint-Regeln, die mit Prettier kollidieren
+]);
